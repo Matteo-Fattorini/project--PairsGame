188597ff7417552e5399a57e1ddf4931f627d540
Matteo Fattorini <fattorinimatteo@gmail.com>
2020-11-28 14:22:29 +0100

    1.0
    The game of Pairs in VUE.js

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const symbols = [
+  "fas fa-crosshairs fa-3x",
+  "fas fa-people-arrows fa-3x",
+  "fas fa-first-aid fa-3x",
+  "fas fa-wrench fa-3x",
+  "fas fa-republican fa-3x",
+  "fas fa-truck-pickup fa-3x",
+  "fas fa-drafting-compass fa-3x",
+  "fas fa-hammer fa-3x",
+  "fas fa-paint-roller fa-3x",
+  "fas fa-screwdriver fa-3x",
+  "fas fa-terminal fa-3x",
+  "fas fa-ship fa-3x",
+];
+
+
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,145 @@
+const app = new Vue({
+  el: ".main-container",
+  data: {
+    difficulty: "", //v-modeled with difficulty select
+    symbols, //symbols class array from data
+    actualSymbols: [], //random symbols from data, based on difficulty
+    game: false, //true when start is pressed
+    currentCell: [],
+    firstStart: true,
+    guessedRight: 0,
+    score: 5000,
+    gameOver: false
+  },
+  computed: {
+    diffSel() {
+      if (this.difficulty == "0") {
+        //how many cell needs to be generated
+        return 12;
+      } else if (this.difficulty == "1") {
+        return 24;
+      }
+    },
+  },
+
+ 
+
+  methods: {
+    checkWinner() {
+      if (this.difficulty == 0 && this.guessedRight >= 12 || this.difficulty == 1 && this.guessedRight >= 24 ) {
+        this.gameOver = true;
+      } 
+    },
+
+    generateSymbols() {
+      //this will generete the actualSymbols variable. Picks random from symbols.
+      if (!this.game && this.difficulty != "") {
+        this.game = true;
+        let index = [];
+        if (this.difficulty == 0) {
+          index = this.randomNums(6);
+        } else {
+          index = this.randomNums(12);
+        }
+
+        for (numbers of index) {
+          this.actualSymbols.push(symbols[numbers]);
+          this.actualSymbols.push(symbols[numbers]);
+        }
+        this.shuffle(this.actualSymbols);
+      }
+    },
+
+    randomNums(n) {
+      //generate a random list of unique "n" numbers
+      var arr = [];
+      while (arr.length < n) {
+        var r = Math.floor(Math.random() * 12);
+        if (arr.indexOf(r) === -1) arr.push(r);
+      }
+      return arr;
+    },
+
+    shuffle(array) {
+      //method that shuffle an array. Used on actualSymbols
+      let currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    },
+
+    setgreen() {
+      if (this.firstStart) {
+        for (let cell of this.$refs.listCell) {
+          cell.classList.add("temporaryDisplay");
+        }
+
+        setInterval(function () {
+          for (let cell of app.$refs.listCell) {
+            this.generating = true;
+            cell.classList.remove("temporaryDisplay");
+          }
+        }, 2000);
+      }
+      this.firstStart = false;
+      let current = this.currentCell;
+
+      if (current.length == 2) {
+        this.currentCell = [];
+
+        currentClass = current[0].innerHTML;   //check if selected box have same symbols.
+
+        if (current[1].innerHTML == currentClass) {
+          current.forEach((element) => element.classList.add("guessedRight"));
+          this.guessedRight += 2
+          this.checkWinner();
+        } else {
+          this.score -= 100
+        }
+
+        setTimeout(function () {
+          //timeout to remove automatically the display block
+          for (let cell of app.$refs.listCell) {
+            cell.classList.remove("currentSelect");
+          }
+        }, 1000);
+      }
+      for (let cell of this.$refs.listCell) {
+        if (
+          cell.tabIndex == event.target.tabIndex &&
+          current.indexOf(cell) == -1
+        ) {
+          current.push(cell);
+          cell.classList.add("currentSelect");
+        }
+      }
+    },
+
+    /**
+     * This Function handles the click. If guessed is < than 2 will add the clicked item to array. Else it
+     * will reset the array and run the cicle again.
+     * @param {event} event
+     */
+
+    selectBox(event) {
+      
+      let current = this.currentCell;
+      if (current.length < 2) {
+        this.setgreen();
+      } else if (current.length == 2) {
+        current = [];
+        for (let cell of this.$refs.listCell) {
+          cell.classList.remove("currentSelect");
+        }
+      }
+      this.setgreen();
+    },
+  },
+});
